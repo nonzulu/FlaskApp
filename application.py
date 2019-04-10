de5705abd11b20bcdeb803644c0b5adf53f6e695
nonzulu kala <nonzulu@kineticskunk.com>
2019-04-10 16:43:04 +0200

    modified

--- application.py
+++ application.py
@@ -2,5 +2,6 @@ from src.flaskbasic import application ,db
 from src.flaskbasic.form import *
 
 
+
 if __name__ == '__main__':
 	application.run(debug=True)
--- src/flaskbasic/database.py
+++ src/flaskbasic/database.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from src.flaskbasic import application
 
+
 application.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///db/bellybutton.sqlite"
 db = SQLAlchemy(application)
 
