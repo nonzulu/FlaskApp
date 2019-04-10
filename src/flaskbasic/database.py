1b2a0822753c65188d8007f67e9ec35e5ccc641c
nonzulu kala <nonzulu@kineticskunk.com>
2019-04-10 16:29:33 +0200

    modified requirements

--- src/flaskbasic/database.py
+++ src/flaskbasic/database.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from src.flaskbasic import application
 
+application.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///db/bellybutton.sqlite"
 db = SQLAlchemy(application)
 
 from src.flaskbasic.wsgi import *
+
+
